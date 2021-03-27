b5b1c982b626e33a968c03f6dac90edc31f8e810
AmanGupta262 <aman@aman>
2021-03-27 10:17:00 +0530

    read tasks from database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,14 @@
 const Task = require('../models/task');
 
 module.exports.home = (req, res) => {
-
+    Task.find({}, (err, tasks) => {
+        if (err) { console.log("Error in finding tasks"); return; }
 
         res.render('home', { 
             title: "ToDo App",
+            tasks: tasks
         });
+    });
 };
 
 module.exports.create = (req, res) => {
