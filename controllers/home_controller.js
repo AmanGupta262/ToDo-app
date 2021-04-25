ea36d1531c85025d80e90c51f05d393380f40c81
Aman Gupta <amang5840@gmail.com>
2021-04-25 11:39:55 +0530

    fix some issues

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,29 +10,3 @@ module.exports.home = (req, res) => {
         });
     });
 };
-
-module.exports.create = (req, res) => {
-    Task.create({
-        description: req.body.description,
-        dueDate: req.body.date,
-        category: req.body.category
-    }, (err, task) => {
-        if(err){ console.log("Error in creating task"); return; }
-
-        return res.redirect('back');
-    });
-}
-
-module.exports.delete = (req, res) => {
-    Task.deleteMany(
-        {
-            _id: {
-                $in: req.body.task
-            }
-        },
-        (err) => {
-            if(err){ console.log("Error in finding task"); return; }
-            return res.redirect('back');
-        }
-    );
-}
